d497b0ae2ee9321aa5d226de98eebfde26c1c11e
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-05-20 22:25:21 +0700

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+//File: scr/components/Home.js
+const Home=()=>{
+    return (
+        <main className="flex-shrink-0">
+            <div className="container">
+                <h1 className="mt-5">Home</h1>
+                <p>Wengdev Community merupakan komunitas codingan g jelas yang didirikan oleh 2 programmer idaman mertua slebew </p>
+            </div>
+        </main>
+    );
+}
+
+export default Home;
---
+++ src/components/Produk.js
@@ -0,0 +1,15 @@
+const Produk = () => {
+  return (
+    <main className="flex-shrink-0">
+      <div className="container">
+        <h1 className="mt-5">Daftar Produk</h1>
+        <hr />
+        <h3>
+          <a href="https://halu-application.vercel.app/">Aplikasi Halu</a>
+        </h3>
+      </div>
+    </main>
+  );
+};
+
+export default Produk;
